3543d71b35cd5e7131d7cd5aa0d64a9cbc8006ba
Lucas Ortis <lucasortis01@gmail.com>
2021-09-19 11:56:49 -0300

    Initial program

---
+++ TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
